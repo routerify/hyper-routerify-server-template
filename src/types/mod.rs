ba02e3d500f538036c31ac326ee2097600aa584c
Rousan Ali <rousanali786@gmail.com>
2020-05-13 19:00:56 +0530

    Update

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct AppInfo {
     pub name: &'static str,
     pub version: &'static str,
